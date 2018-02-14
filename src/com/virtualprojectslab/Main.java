520f892635c25118c6fa99f170e4264745292d51
buhtelj <andrej.cijan@gmail.com>
2018-02-14 13:09:42 +0100

    konec lekcije

--- src/com/virtualprojectslab/Main.java
+++ src/com/virtualprojectslab/Main.java
@@ -3,6 +3,17 @@ package com.virtualprojectslab;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //width of 16 bits
+        //room for one character
+        //code comes from https://unicode-table.com
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBooleanTrue = true;
+        boolean myBooleanFalse = false;
+
+        char myRegSymbol = '\u00AE';
+        System.out.println(myRegSymbol);
+
     }
 }
